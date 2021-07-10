b935b448d9e9efef5f8ae2d8723258e3cb2d4711
Anh Vuong <vta19797@gmail.com>
2021-07-09 23:48:33 -0500

    testing js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert that JavaScript is running");
+var playerName= window.prompt ("What is your robot's name?");
+//What is it?
+console.log (playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+
+console.log("our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight (){
+    window.alert("The fight has begun");
+}
+
+//fight();
