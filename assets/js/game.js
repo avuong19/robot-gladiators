8e23646f9e96e951ed5a162a46c4f3c9d6dcf481
Anh Vuong <vta19797@gmail.com>
2021-07-11 19:50:39 -0500

    Batttle the combatant until defeated using a while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,10 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
+    
+    //repeat and execute as long as the enemy robot is alive
+    while (enemyHealth>0){
+    
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or Skip to choose");
@@ -67,10 +71,14 @@ var fight = function(enemyName) {
     else{
         window.alert("You need to choose a valid option. Try again!");
     }
+}
 
 };
 
 for (var i=0; i< enemyNames.length; i++){
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth=50;
+    //call fight function with enemy robot 
     fight(enemyNames[i]);
 }
  
